1f53d287b87e96e83f634bdf4eb66b32d174ddf6
suphotsudsee <suphotsudsee@gmail.com>
2026-03-27 16:13:35 +0700

    test

--- src/auth.ts
+++ src/auth.ts
@@ -10,6 +10,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   pages: {
     signIn: "/login",
   },
+  trustHost: true,
   providers: [
     Credentials({
       name: "credentials",
